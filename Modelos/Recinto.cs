75f1e04d68daf53ac1aa680716a897597912f1c5
AntonioRamonRomero <149623480+AntonioRamonRomero@users.noreply.github.com>
2025-04-28 21:02:47 -0300

    Add files via upload
    Modelo de Recinto Agregado

---
+++ Modelos/Recinto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Recinto
+    {
+        public int IdRecinto { get; set; }
+        public int NumeroRecinto { get; set; }
+        public decimal Tarifa { get; set; }
+        public string Ubicacion { get; set; }
+        public bool Habilitado { get; set; }
+        public Tipo_De_Recinto TipoRecinto { get; set; }
+    }
+}
