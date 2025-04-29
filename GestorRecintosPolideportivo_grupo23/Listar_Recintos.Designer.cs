a78b0469a88b937542984f5bd0d7c10b8d3e3e2a
AntonioRamonRomero <149623480+AntonioRamonRomero@users.noreply.github.com>
2025-04-28 21:17:31 -0300

    Add files via upload
    Vista de Listar Recinto

---
+++ GestorRecintosPolideportivo_grupo23/Listar_Recintos.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Listar_Recintos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewRecintos = new System.Windows.Forms.DataGridView();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecintos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewRecintos
+            // 
+            this.dataGridViewRecintos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRecintos.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewRecintos.Name = "dataGridViewRecintos";
+            this.dataGridViewRecintos.Size = new System.Drawing.Size(776, 357);
+            this.dataGridViewRecintos.TabIndex = 0;
+            this.dataGridViewRecintos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRecintos_CellContentClick);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(426, 401);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(90, 37);
+            this.btnVolver.TabIndex = 2;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Location = new System.Drawing.Point(272, 401);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(90, 37);
+            this.btnRefrescar.TabIndex = 1;
+            this.btnRefrescar.Text = "Refrescar";
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            this.btnRefrescar.Click += new System.EventHandler(this.btnRefrescar_Click);
+            // 
+            // Listar_Recintos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnRefrescar);
+            this.Controls.Add(this.dataGridViewRecintos);
+            this.Name = "Listar_Recintos";
+            this.Text = "Listar Recintos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecintos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewRecintos;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnRefrescar;
+    }
+}
