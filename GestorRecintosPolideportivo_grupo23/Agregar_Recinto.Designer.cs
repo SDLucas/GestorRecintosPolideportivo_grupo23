5831e79482ae83163e37eb07d83485436b2a2b75
NotANumberMan <nahuelr298@outlook.com>
2025-04-28 21:06:40 -0300

    Vista de Agregar Recinto

---
+++ GestorRecintosPolideportivo_grupo23/Agregar_Recinto.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Agregar_Recinto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtTarifa = new System.Windows.Forms.TextBox();
+            this.txtUbicacion = new System.Windows.Forms.TextBox();
+            this.cbTipoRecinto = new System.Windows.Forms.ComboBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblTarifa = new System.Windows.Forms.Label();
+            this.lblUbicacion = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 25);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(256, 20);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // txtTarifa
+            // 
+            this.txtTarifa.Location = new System.Drawing.Point(12, 68);
+            this.txtTarifa.Name = "txtTarifa";
+            this.txtTarifa.Size = new System.Drawing.Size(256, 20);
+            this.txtTarifa.TabIndex = 1;
+            // 
+            // txtUbicacion
+            // 
+            this.txtUbicacion.Location = new System.Drawing.Point(12, 111);
+            this.txtUbicacion.Name = "txtUbicacion";
+            this.txtUbicacion.Size = new System.Drawing.Size(256, 20);
+            this.txtUbicacion.TabIndex = 2;
+            // 
+            // cbTipoRecinto
+            // 
+            this.cbTipoRecinto.FormattingEnabled = true;
+            this.cbTipoRecinto.Location = new System.Drawing.Point(12, 154);
+            this.cbTipoRecinto.Name = "cbTipoRecinto";
+            this.cbTipoRecinto.Size = new System.Drawing.Size(256, 21);
+            this.cbTipoRecinto.TabIndex = 3;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(12, 9);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(94, 13);
+            this.lblNumero.TabIndex = 4;
+            this.lblNumero.Text = "Numero de recinto";
+            // 
+            // lblTarifa
+            // 
+            this.lblTarifa.AutoSize = true;
+            this.lblTarifa.Location = new System.Drawing.Point(12, 52);
+            this.lblTarifa.Name = "lblTarifa";
+            this.lblTarifa.Size = new System.Drawing.Size(76, 13);
+            this.lblTarifa.TabIndex = 5;
+            this.lblTarifa.Text = "Tarifa por hora";
+            // 
+            // lblUbicacion
+            // 
+            this.lblUbicacion.AutoSize = true;
+            this.lblUbicacion.Location = new System.Drawing.Point(12, 95);
+            this.lblUbicacion.Name = "lblUbicacion";
+            this.lblUbicacion.Size = new System.Drawing.Size(55, 13);
+            this.lblUbicacion.TabIndex = 6;
+            this.lblUbicacion.Text = "Ubicacion";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(12, 138);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(78, 13);
+            this.lblTipo.TabIndex = 7;
+            this.lblTipo.Text = "Tipo de recinto";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(99, 198);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(82, 38);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Agregar_Recinto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 257);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblUbicacion);
+            this.Controls.Add(this.lblTarifa);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.cbTipoRecinto);
+            this.Controls.Add(this.txtUbicacion);
+            this.Controls.Add(this.txtTarifa);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Agregar_Recinto";
+            this.Text = "Agregar Recinto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtTarifa;
+        private System.Windows.Forms.TextBox txtUbicacion;
+        private System.Windows.Forms.ComboBox cbTipoRecinto;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblTarifa;
+        private System.Windows.Forms.Label lblUbicacion;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
