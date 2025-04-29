351b8cfa32f39622549442b675e9a409aad6f1d7
SFRisso <santiago.risso2@gmail.com>
2025-04-28 21:03:45 -0300

    Agregar tipo_De_Recinto.cs

---
+++ Modelos/Tipo_De_Recinto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Tipo_De_Recinto
+    {
+        public int id_tipo_recinto { get; set; }
+        public string nombre_tipo_recinto { get; set; }
+
+        public override string ToString()
+        {
+            return this.nombre_tipo_recinto;
+        }
+    }
+}
